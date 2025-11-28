b6617a1b81e4d82b55207f2339a6b2b9db9725b4
Markus Thilo <markus.thilo@gmail.com>
2025-11-28 11:16:52 +0100

    Version 1.0.0 RC1

--- cirdan.py
+++ cirdan.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Markus Thilo'
-__version__ = '0.9.9_2025-11-24'
+__version__ = '1.0.0_2025-11-28'
 __license__ = 'GPL-3'
 __email__ = 'markus.thilo@gmail.com'
 __status__ = 'Testing'
--- make-dist.bat
+++ make-dist.bat
@@ -1,27 +1,15 @@
 @echo off
 echo Building Cirdan distribution...
-
-REM Clean up any existing distribution folders
-if exist cirdan.dist (
-    echo Cleaning existing distribution folder cirdan.dist...
-    del cirdan.dist\* /Q
-)
-if exist download_app.dist (
-    echo Cleaning existing distribution folder cirdan.dist...
-    del download_app.dist\* /Q
-)
-
-REM Build the standalone executable with Nuitka
+if exist cirdan.dist rmdir cirdan.dist /S /Q
+if exist download_app.dist rmdir download_app.dist /S /Q
 echo Building executable with Nuitka...
-call python -m nuitka --windows-icon-from-ico=appicon.ico --windows-console-mode=disable --standalone --enable-plugin=tk-inter cirdan.py
-call python -m nuitka --standalone download_app.py
-del cirdan.build /Q
-del download_app.build /Q
-
-echo Distribution build complete!
-echo Files are available in the cirdan.dist directory.
-
+call python -m nuitka --msvc=latest --windows-icon-from-ico=appicon.ico --windows-console-mode=disable --standalone --enable-plugin=tk-inter cirdan.py
+call python -m nuitka --msvc=latest --standalone download_app.py
+rmdir cirdan.build /S /Q
+rmdir download_app.build /S /Q
 echo Writing Version number to file version.txt
 cirdan.dist\cirdan.exe -v > version.txt
-
-echo All done.
+set /P Version=<version.txt
+mkdir ..\Cirdan_v%Version%
+download_app.dist\download_app.exe .\ ..\Cirdan_v%Version%
+echo Distribution build complete!
